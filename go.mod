9bb466a7e65aab4b9ca43d7576e227bebca71f20
Ryan Travitz <rtravitz@gmail.com>
2019-04-11 11:02:13 -0400

    Use oauth2 client

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/rtravitz/va-api-client
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.1
+require (
+	github.com/coreos/go-oidc v2.0.0+incompatible
+	github.com/gorilla/mux v1.7.1
+	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
+	golang.org/x/crypto v0.0.0-20190404164418-38d8ce5564a5 // indirect
+	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a
+	gopkg.in/square/go-jose.v2 v2.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,48 +1,78 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/oauth2"
 )
 
-const callback = "https://go-va-api-client.herokuapp.com/auth/callback"
-
-func redirectHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Incoming request:\n%+v\n", r)
-	w.Write([]byte("redirect route"))
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	u, err := url.Parse("https://dev-api.va.gov/oauth2/authorization")
-	if err != nil {
-		log.Println("Error parsing URL", err)
-		return
-	}
-
-	q := u.Query()
-	q.Add("client_id", os.Getenv("VA_OAUTH_CLIENT_ID"))
-	q.Add("redirect_uri", callback)
-	q.Add("response_type", "code")
-
-	u.RawQuery = q.Encode()
-	fmt.Println(u.String())
-
-	http.Redirect(w, r, u.String(), http.StatusFound)
-}
+var (
+	clientID     = os.Getenv("VA_OAUTH_CLIENT_ID")
+	clientSecret = os.Getenv("VA_OAUTH_CLIENT_SECRET")
+	redirectURL  = "https://go-va-api-client.herokuapp.com/auth/callback"
+)
 
 func main() {
-	fs := http.FileServer(http.Dir("static"))
+	ctx := context.Background()
+
+	config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://dev-api.va.gov/oauth2/authorization",
+			TokenURL: "https://dev-api.va.gov/oauth2/token",
+		},
+		RedirectURL: redirectURL,
+		Scopes:      []string{"openid", "profile", "email"},
+	}
+
+	state := "foobar"
+
 	r := mux.NewRouter()
 
 	s := r.PathPrefix("/auth").Subrouter()
-	s.HandleFunc("/login", loginHandler)
-	s.HandleFunc("/callback", redirectHandler)
+	s.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		authURL := config.AuthCodeURL(state)
+		fmt.Println("AUTH URL", authURL)
+		http.Redirect(w, r, config.AuthCodeURL(state), http.StatusFound)
+	})
 
+	s.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != state {
+			http.Error(w, "state did not match", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		oauth2Token.AccessToken = "*REDACTED*"
+
+		resp := struct {
+			OAuth2Token   *oauth2.Token
+			IDTokenClaims *json.RawMessage // ID Token payload is just JSON.
+		}{oauth2Token, new(json.RawMessage)}
+
+		data, err := json.MarshalIndent(resp, "", "    ")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(data)
+	})
+
+	fs := http.FileServer(http.Dir("static"))
 	r.PathPrefix("/").Handler(fs)
 
 	port := os.Getenv("PORT")
